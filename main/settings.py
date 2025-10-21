161194a1b0f1c208759cc3907b19d06b50715d3a
saumyata818 <saumyata.nepal@gmail.com>
2025-10-21 19:09:19 +0545

    Fix runtime

--- main/settings.py
+++ main/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['your-railway-app.up.railway.app', 'localhost']
 
 # Application definition
 INSTALLED_APPS = [
